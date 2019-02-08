c6cf7b57f8487cf4175b1e42367d1d69dbb105ed
Aliya Summers <aliyasummers1@gmail.com>
2019-02-07 22:35:35 -0800

    I have questions 1 through 6 working, still figuring out 7

--- js/app.js
+++ js/app.js
@@ -1,138 +1,182 @@
 'use strict';
-//Erik Enderlein helped with .toLowerCase placement
 //declaring variable to use in answer field rather than strings
-var answerYes = 'yes';
-var answerY = 'y';
-var answerNo = 'no';
-var answerN = 'n';
+var answerYes = ['yes', 'YES', 'y', 'Y', 'ye', 'Yes'];
+var answerNo = ['no', 'NO', 'n', 'N', 'nope'];
 
 
-
-/* START OF QUESTION 1*/
-
-//pop up for question 1
-var answer = prompt('Do I like mayonnaisse?');
-
-//if yes/y
-if (answer.toLowerCase() === answerYes) {
-  alert('You are very wrong');
-
-} else if (answer.toLowerCase() === answerY) {
-  alert('wrong, but you saved time on typing');
-
-//if no/n
-} else if (answer.toLowerCase() === answerNo) {
-  alert('Correct! I hate it');
-
-} else if (answer.toLowerCase() === answerN) {
-  alert('you are right');
-
-//other answers
-} else {
-  alert('please do not be like this');
+//prompt for question 1 of 5 y/n Qs
+//options for Yes answer
+var answer = prompt('Have I ever willingly consumed mayonnaise?');
+for(var i = 0; i <= answerYes.length; i++){
+  if (answerYes[i] === answer){
+    console.log('answer: ' + answer + '. This answer is wrong');
+    alert('You are wrong');
+  }else{
+  //I have this log here so that if someone (most likely me) decides to
+  //keyboard-smash/other invalid input, I can still see what they wrote
+    console.log(answer);
+  }
 }
-console.log('answer to question 1: ' + answer);
 
+//options for No answer
+for(i = 0; i <= answerNo.length; i++){
+  if (answerNo[i] === answer){
+    console.log(answer + ' was right, plus 1 pt');
+    alert('You are right. I hate the stuff');
+  }
+}
 
-/*START OF QUESTION 2*/
-
-//pop up from q2
+//prompt for question 2 of 5 y/n
 answer = prompt('Was my first pet a cat?');
-
-//if yes/y
-if (answer.toLowerCase() === answerYes) {
-  alert('you are wrong but cats are cool');
-
-} else if (answer.toLowerCase() === answerY) {
-  alert('Wrong, but I would like a cat');
-
-//if no/n
-} else if (answer.toLowerCase() === answerNo) {
-  alert('You are right I had a dog');
-
-} else if (answer.toLowerCase() === answerN) {
-  alert('the dog\'s name was short but still longer than your answer');
-
-//other answers
-} else {
-  alert('why are you like this?');
+for( i = 0; i <= answerYes.length; i++){
+  if (answerYes[i] === answer){
+    console.log('answer: ' + answer + '. This answer is wrong');
+    alert('You are wrong');
+  }else{
+  //I have a log for invalid answers
+    console.log(answer);
+  }
 }
-console.log('answer to Q2: ' + answer);
 
-
-/*START OF QUESTION 3*/
-
-//pop up from q3
-answer = prompt(' Have I lived outside Washington?');
-
-//if yes/y
-if (answer.toLowerCase() === answerYes) {
-  alert('Correct!');
-
-} else if (answer.toLowerCase() === answerY) {
-  alert('You are right');
-
-//if no/n
-} else if (answer.toLowerCase() === answerNo) {
-  alert('Wrong');
-
-} else if (answer.toLowerCase() === answerN) {
-  alert('Wrong, but I have been up here a few years');
-
-//other answers
-} else {
-  alert('it\'s a simple question why didn\'t you answer it');
+//options for No answer
+for(i = 0; i <= answerNo.length; i++){
+  if (answerNo[i] === answer){
+    console.log(answer + ' was right, plus 1 pt');
+    alert('You are right, but I think they are cute');
+  }
 }
-console.log('answer to Q3: ' + answer);
 
-
-/*START OF QUESTION 4*/
-
-//pop up from q4
-answer = prompt(' Have I visited the moon?');
-
-//if yes/y
-if (answer.toLowerCase() === answerYes) {
-  alert('Wrong');
-
-} else if (answer.toLowerCase() === answerY) {
-  alert('Nope, but lots of respect for those who have');
-
-//if no/n
-} else if (answer.toLocaleLowerCase() === answerNo) {
-  alert('You are right');
-
-} else if (answer.toLowerCase() === answerN) {
-  alert('Correct, I have no visited the moon');
-
-//other answers
-} else {
-  alert('how dare you disrespect the space lesbian like this');
+//prompt for question 3 of 5 y/n
+answer = prompt('Do I have a collection of interesting rocks?');
+for( i = 0; i <= answerYes.length; i++){
+  if (answerYes[i] === answer){
+    console.log('answer: ' + answer + '. This answer is right');
+    alert('You are right. Rocks are cool');
+  }else{
+  //I have a log for invalid answers
+    console.log(answer);
+  }
 }
-console.log('answer to Q4: ' + answer);
 
-
-/*START OF QUESTION 5*/
-
-//pop up from q5
-answer = prompt(' Do I think grizzly bears are neat?');
-
-//if yes/y
-if (answer.toLowerCase() === answerYes) {
-  alert('Correct. They are interesting animals');
-
-} else if (answer.toLowerCase() === answerY) {
-  alert('They could eat me, but I still think they\'re cool');
-
-//if no/n
-} else if (answer.toLowerCase() === answerNo) {
-  alert('You are wrong but don\'t feel bad');
-
-} else if (answer.toLowerCase() === answerN) {
-  alert('Wrooong. Restart from the top and try again');
-
-//other answers
-} else {
-  alert('are you actually a bear? is that why you can\'t answer my question?');
+//options for No answer
+for(i = 0; i <= answerNo.length; i++){
+  if (answerNo[i] === answer){
+    console.log(answer + ' was right, plus 1 pt');
+    alert('You are wrong, and might need more interesting rocks');
+  }
 }
-console.log('answer to Q5: ' + answer);
+
+//prompt for question 4 of 5 y/n
+answer = prompt('Have I lived on the east coast?');
+for( i = 0; i <= answerYes.length; i++){
+  if (answerYes[i] === answer){
+    console.log('answer: ' + answer + '. This answer is wrong');
+    alert('Wrong, but I have family there.');
+  }else{
+  //I have a log for invalid answers
+    console.log(answer);
+  }
+}
+
+//options for No answer
+for(i = 0; i <= answerNo.length; i++){
+  if (answerNo[i] === answer){
+    console.log(answer + ' was right, plus 1 pt');
+    alert('You are right, but I still have family there');
+  }
+}
+
+//prompt for question 5 of 5 y/n
+answer = prompt('Have you done the reading?');
+for( i = 0; i <= answerYes.length; i++){
+  if (answerYes[i] === answer){
+    console.log('answer: ' + answer + '. goood answer');
+    alert('Congratulations on being a good student');
+  }else{
+  //I have a log for invalid answers
+    console.log(answer);
+  }
+}
+
+//options for No answer
+for(i = 0; i <= answerNo.length; i++){
+  if (answerNo[i] === answer){
+    console.log(answer + ' wrong');
+    alert('Well you should do the reading');
+  }
+}
+
+//Pick a number question. I have picked 9
+
+var pickNumber = [9, 9, 9, 9];
+var pickNumberAnswer = prompt('See if you can guess the number I\'m thinking of. It is between 1 and 15');
+/*
+This works because it iterates through the pickNumber array, which I have used to limit how many times the
+question runs.
+Note: This only works because I have set my entire array to the same numer. It is picking an different index,
+or a different 9, each time. If I were to change the numbers past index [0] to anything other than 9, the questions
+wouldn't work properly. While this works, it is far from a perfect fix.
+*/
+for(i = 0; i <= pickNumber.length; i++){
+  if (pickNumberAnswer > pickNumber[i]){
+    console.log(pickNumberAnswer + ' was too big');
+    pickNumberAnswer = prompt('Too big');
+  }else if(pickNumberAnswer < pickNumber[i]){
+    console.log(pickNumberAnswer + ' was too small');
+    pickNumberAnswer = prompt('Too small');
+  }else if(pickNumberAnswer == pickNumber[i]){
+    console.log(pickNumberAnswer + ' was correct');
+    alert('Congrats you got it');
+    break;
+  }
+}
+
+
+//solving 7 with similar thing to yes/no questions;
+var faveAnimals = ['bears', ' orangutans', 'dogs'];
+//var faveAnimalsAnswer = prompt('What are some my favorite animals');
+
+var faveAnimalsAnswer = prompt('What are some of my favorite animals?');
+for( i = 0; i < answerYes.length; i++){
+  if (faveAnimals[i] !== faveAnimalsAnswer){
+    console.log('answer: ' + faveAnimalsAnswer + '. This answer is wrong!');
+    alert('You are wrong');
+  }
+}
+
+for( i = 0; i < answerYes.length; i++){
+  if (faveAnimals[i] === faveAnimalsAnswer){
+    console.log('answer: ' + faveAnimalsAnswer + '. This answer is right!');
+    alert('You are right');
+    break;
+  }
+}
+
+
+
+
+
+//Question 7
+//Should be able to do a similar thing to #6
+//Question: what are some of my favorite animals?
+
+//var faveAnimals = ['bears', ' orangutans', 'dogs'];
+/*var faveAnimals =['bears', 'dogs'];
+var faveAnimalsAnswer = prompt('What are some my favorite animals');
+
+//in its current state it only accepts answers if they align with their position on the array. How fix?
+
+for(i = 0; i < faveAnimals.length; i++){
+  if (faveAnimals[i] !== faveAnimalsAnswer){
+    console.log(faveAnimalsAnswer + ' was wrong.');
+    faveAnimalsAnswer = prompt('Try again');
+  } else if(faveAnimals[i] === faveAnimalsAnswer){
+    console.log(faveAnimalsAnswer + ' was correct');
+    alert('Congrats! That is one of my favorite animals. Here is the full list of the big ones ' + faveAnimals);
+  }
+}*/
+
+//at the end, right before break, list the answers
+//comparing input at index of array. If the user input is in the array, i
+//should return true, or as long as the user put said something in the array, it will give right alert
+
