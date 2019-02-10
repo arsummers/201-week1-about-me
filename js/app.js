d883796b0b276ff9926659914735f806ac80d5cd
Aliya Summers <aliyasummers1@gmail.com>
2019-02-10 11:25:01 -0800

    added score counter, need to fix css

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
 'use strict';
-/* FOR
-THE
-LOVE
-OF
-GOD
-UPDATE
-YOUR
-README*/
-//declaring variable to use in answer field rather than strings
 
 //Question 1: y/n.
+
+//keeps track of right guesses
+var counter = 0;
+
+//keeps track of wrong guesses
+var wrongGuessNumber = 0;
 
 function questionOne() {
   var answerYes = ['yes', 'y'];
@@ -19,28 +16,38 @@ function questionOne() {
   var questionOneInput = prompt('Have I ever willingly consumed mayonnaise?');
   for (var i = 0; i < questionOneGuesses; i++) {
     if (answerNo[0] === questionOneInput.toLowerCase()) {
-      console.log(questionOneInput + 'was right');
+      console.log(questionOneInput + ' was right');
       alert('You are right. I hate mayo');
+      counter ++;
+      console.log('score: ' + counter);
       break;
     } else if (answerNo[1] === questionOneInput.toLowerCase()) {
-      console.log(questionOneInput + 'was right');
+      console.log(questionOneInput + ' was right');
       alert('You are right. I hate mayo');
+      counter++;
+      console.log('score: '+ counter);
       break;
     } else if (answerYes[0] === questionOneInput.toLowerCase()) {
       console.log(questionOneInput + ' was wrong');
       alert('That was wrong. Mayo is vile.');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
       break;
     } else if (answerYes[1] === questionOneInput.toLowerCase()) {
       console.log(questionOneInput + ' was wrong');
       alert('That was wrong. Mayo is vile.');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
       break;
     } else if (i === 1) {
       console.log(questionOneInput + ' was an invalid answer');
       alert(questionOneInput + ' was not a valid answer. Too bad you only had one shot at it');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
     }
   }
 }
-//makes the function go
+//calls question 1
 questionOne();
 
 //Question 2, yes/no
@@ -53,22 +60,32 @@ function questionTwo(){
     if (answerYes[0] === questionTwoInput.toLowerCase()) {
       console.log(questionTwoInput + ' was right');
       alert('You are right! I got a black lab puppy from the pound, and named him Pups because I was six at the time.');
+      counter++;
+      console.log('score: ' + counter);
       break;
     } else if (answerYes[1] === questionTwoInput.toLowerCase()) {
       console.log(questionTwoInput + ' was right');
       alert('You are right!  I got a black lab puppy from the pound, and named him Pups because I was six at the time.');
+      counter++;
+      console.log('score: ' + counter);
       break;
     } else if (answerNo[0] === questionTwoInput.toLowerCase()) {
       console.log(questionTwoInput + ' was wrong');
       alert('You are wrong. My first pet was a sweet rescue puppy, but you don\'t get to know his name because you gave the wrong answer');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
       break;
     } else if (answerNo[1] === questionTwoInput.toLowerCase()) {
       console.log(questionTwoInput + ' was wrong');
       alert('You are wrong. My first pet was a sweet rescue puppy, but you don\'t get to know his name because you gave the wrong answer');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
       break;
     } else if (a === 1) {
       console.log(questionTwoInput + ' was invalid');
       alert(questionTwoInput + ' wasn\'t a valid answer at all. My first pet was a dog, but you need to give better answers');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
     }
   }
 }
@@ -88,22 +105,32 @@ function questionThree() {
     if (answerNo[0] === questionThreeInput.toLowerCase()) {
       console.log(questionThreeInput + 'was was wrong');
       alert('You\'re wrong. I was lucky enough to watch it');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
       break;
     } else if (answerNo[1] === questionThreeInput.toLowerCase()) {
       console.log(questionThreeInput + 'was wrong');
       alert('You\'re wrong. I was lucky enough to watch it');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
       break;
     } else if (answerYes[0] === questionThreeInput.toLowerCase()) {
       console.log(questionThreeInput + ' was right');
       alert('You are right! The night was even clear enough for it');
+      counter++;
+      console.log('score: ' + counter);
       break;
     } else if (answerYes[1] === questionThreeInput.toLowerCase()) {
       console.log(questionThreeInput + ' was right');
       alert('You are right! The night was even clear enough for it');
+      counter++;
+      console.log('score: ' + counter);
       break;
     } else if (b === 1) {
       console.log(questionThreeInput + ' was an invalid answer');
       alert(questionThreeInput + ' was not a valid answer. Pretty rude of you to give a bad answer. The moon deserves better.');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
     }
   }
 }
@@ -121,22 +148,31 @@ function questionFour() {
     if (answerNo[0] === questionFourInput.toLowerCase()) {
       console.log(questionFourInput + ' was wrong');
       alert('That was not nice to BEARS???');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
       break;
     } else if (answerNo[1] === questionFourInput.toLowerCase()) {
       console.log(questionFourInput + ' was wrong');
       alert('That was not nice to BEARS???');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
       break;
     } else if (answerYes[0] === questionFourInput.toLowerCase()) {
       console.log(questionFourInput + ' was right');
       alert('Yes bears! They have cute noses');
+      counter++;
+      console.log('score: ' + counter);
       break;
     } else if (answerYes[1] === questionFourInput.toLowerCase()) {
       console.log(questionFourInput + ' was right');
-      alert('Yes bears! They have cute noses');
+      alert('Yes bears! They have cute noses');counter++;
+      console.log('score: ' + counter);
       break;
     } else if (c === 1) {
       console.log(questionFourInput + ' was an invalid answer');
       alert(questionFourInput + ' probably wasn\'t a valid answer. You should know better by now.');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
     }
   }
 }
@@ -153,22 +189,32 @@ function questionFive(){
     if (answerYes[0] === questionFiveInput.toLowerCase()) {
       console.log(questionFiveInput + ' they did the reading');
       alert('Congrats on being a diligent student!');
+      counter++;
+      console.log('score: ' + counter);
       break;
     } else if (answerYes[1] === questionFiveInput.toLowerCase()) {
       console.log(questionFiveInput + ' they did the reading');
       alert('Congrats on being a diligent student!');
+      counter++;
+      console.log('score: ' + counter);
       break;
     } else if (answerNo[0] === questionFiveInput.toLowerCase()) {
       console.log(questionFiveInput + ' they did not do the reading');
       alert('You should do the reading');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
       break;
     } else if (answerNo[1] === questionFiveInput.toLowerCase()) {
       console.log(questionFiveInput + ' they did not do the reading');
       alert('You should do the reading');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
       break;
     } else if (d === 1) {
       console.log(questionFiveInput + ' was invalid');
       alert(questionFiveInput + ' wasn\'t a valid answer. Have you considered doing the reading?');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
     }
   }
 }
@@ -181,24 +227,36 @@ function questionSix() {
   var guesses = 3;
   var pickNumberAnswer = prompt('See if you can guess the number I\'m thinking of. It is between 1 and 15');
   for (var p = 0; p < guesses; p++) {
+    //this if is set to a loose equals, because when it was set to strong equals
+    //the prompt didn't recognize user input because it wanted a string, and a string of
+    //9 wasn't considered a number
     if (pickNumberAnswer == pickNumber){
       console.log(pickNumberAnswer + ' was right');
       alert('Congrats you got it');
+      counter++;
+      console.log('score: ' + counter);
       break;
     }else if(p === 2){
       console.log(pickNumberAnswer + ' was wrong. Out of tries');
       alert(pickNumberAnswer + ' was wrong. You are out of tries. Please move on or refresh the page');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
       break;
     }
     else if (pickNumberAnswer > pickNumber){
       console.log(pickNumberAnswer + ' was too big');
       pickNumberAnswer = prompt('Too big');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
     } else if (pickNumberAnswer < pickNumber) {
       console.log(pickNumberAnswer + ' was too small');
       pickNumberAnswer = prompt('Too small');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
     }
   }
 }
+//calls question 6
 questionSix();
 
 //Start of question 7, which gives an array of animals to guess, and only
@@ -213,23 +271,37 @@ function questionSeven() {
     if(weirdAnimals[0] === questionSevenInput.toLowerCase()){
       console.log(questionSevenInput + ' was right');
       alert(questionSevenInput + ' was correct! The rest of the list is ' + weirdAnimals[1] + ' and ' + weirdAnimals[2]);
+      counter++;
+      console.log('score: ' + counter);
       break;
     } else if (weirdAnimals[1] === questionSevenInput.toLowerCase()){
       console.log(questionSevenInput + ' was right');
       alert(questionSevenInput + ' was correct! The rest of the list is ' + weirdAnimals[0] + ' and ' + weirdAnimals[2]);
+      counter++;
+      console.log('score: ' + counter);
       break;
     } else if (weirdAnimals[2] === questionSevenInput.toLowerCase()){
       alert(questionSevenInput + ' was correct! The rest of the list is ' + weirdAnimals[0] + ' and ' + weirdAnimals[1]);
+      counter++;
+      console.log('score: ' + counter);
       break;
     } else if (j === 5) {
       console.log(questionSevenInput + ' was wrong');
       alert(questionSevenInput + ' might still be a cool animal, but unfortunately, you are out of guesses. My favorites are ' + weirdAnimals[0] + ', ' + weirdAnimals[1] + ', and ' + weirdAnimals[2] + '.');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
       break;
     } else {
       console.log(questionSevenInput + ' was incorrect');
       questionSevenInput = prompt(questionSevenInput + ' is probably really neat, but it didn\'t make my list. Try again');
+      wrongGuessNumber++;
+      console.log('wrong guesses: ' + wrongGuessNumber);
     }
   }
 }
 
+//calls question 7
 questionSeven();
+
+//lets you know how many guesses you took and your final score
+alert('You have reached the end! It took you ' + wrongGuessNumber + ' guesses to get here. You\'re final score is: ' + counter + ' of 7.');
